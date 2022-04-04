e1aceb5b6942c7be2a34668fc64281996a82288a
Aditya Vithaldas <aditya.vithaldas@gmail.com>
2022-04-04 17:56:08 +0000

    Added the client code as well

---
+++ RaspberryPiClient/controller.py
@@ -0,0 +1,15 @@
+import  RPi.GPIO as GPIO
+import requests
+
+GPIO.setwarnings(False)
+
+ledPin = 3
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(ledPin, GPIO.OUT)
+
+while 1:
+    command  = requests.get("http://internetRemote.adityavithaldas.repl.co/getcontrol").text
+    if command == "up":
+        GPIO.output(ledPin, GPIO.HIGH)
+    elif command == "stop":
+        GPIO.output(ledPin, GPIO.LOW)
