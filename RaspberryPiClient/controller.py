94f9bec16e9fd513b0fabbebe0fcdf3c6d7a70b7
Aditya Vithaldas <aditya.vithaldas@gmail.com>
2022-04-04 18:21:17 +0000

    Just start and stop

--- RaspberryPiClient/controller.py
+++ RaspberryPiClient/controller.py
@@ -9,7 +9,7 @@ GPIO.setup(ledPin, GPIO.OUT)
 
 while 1:
     command  = requests.get("http://internetRemote.adityavithaldas.repl.co/getcontrol").text
-    if command == "up":
+    if command == "start":
         GPIO.output(ledPin, GPIO.HIGH)
     elif command == "stop":
         GPIO.output(ledPin, GPIO.LOW)
