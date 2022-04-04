128cecc1866f3a6a470cf8945eb337fdcd2aeac4
AdityaVithaldas <aditya.vithaldas@gmail.com>
2022-04-04 17:44:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+app = Flask('app')
+
+@app.route('/')
+def hello_world():
+  return render_template('start.html')
+
+@app.route('/buttonclick')
+def button_click():
+  name = request.args.get("function")
+  f = open("file.txt", "w")
+  f.write(name)
+  f.close()
+  return "file is written"
+
+@app.route('/getcontrol')
+def get_control():
+  f = open("file.txt", "r")
+  ret = f.read()
+  f.close()
+  return ret
+  
+app.run(host='0.0.0.0', port=8080)
